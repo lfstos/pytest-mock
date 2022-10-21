4ac392dc5550f2a7e97ee468dc32341a483b3089
Luiz Fernando <luiz-fernando@outlook.com>
2022-10-21 17:55:41 -0300

    :rocket: Criada classe CanalEmail, criados métodos da classe EnviadorDeSpam, __init__.py, adicionar_destinatario e enviar_spam

--- brincadeira_com_mock/enviador_de_spam.py
+++ brincadeira_com_mock/enviador_de_spam.py
@@ -1,2 +1,18 @@
+class CanalEmail:
+    def enviar(self, destinatario, msg):
+        nome = destinatario.split('@')[0].capitalize()
+        return destinatario, f'{nome} {msg}'
+
 class EnviadorDeSpam:
-    pass
+    def __init__(self, destinatarios=None):
+        self.destinatarios = destinatarios if destinatarios else []
+        self.canal_de_envio = CanalEmail()
+
+    def adicionar_destinatario(self, destinatario):
+        self.destinatarios.append(destinatario)
+
+    def enviar_spam(self, msg):
+        canal_de_envio = CanalEmail()
+        for destinatario in self.destinatarios:
+                # for canal in self.canal_de_envio:
+                    yield canal_de_envio.enviar(destinatario, msg)
