7004e2d2b9c2e70eb2e1f73137f39e78a0bd06c6
Luiz Fernando <luiz-fernando@outlook.com>
2022-10-21 17:57:35 -0300

    :rocket: criada função test_envio_de_spa,

--- brincadeira_com_mock/test_enviador_de_spam.py
+++ brincadeira_com_mock/test_enviador_de_spam.py
@@ -10,3 +10,12 @@ def test_enviador_possui_destinatarios():
     assert enviador.destinatarios == ['luiz@email.com']
     enviador.adicionar_destinatario('fernando@email.com')
     assert enviador.destinatarios == ['luiz@email.com', 'fernando@email.com']
+
+def test_envio_de_spam():
+    enviador = EnviadorDeSpam(['luiz@email.com', 'fernando@email.com'])
+    assert list(
+        enviador.enviar_spam('se liga que vou ficar bom em desenvolvimento com Python!')) == \
+            [
+                ('luiz@email.com', 'Luiz se liga que vou ficar bom em desenvolvimento com Python!'),
+                ('fernando@email.com', 'Fernando se liga que vou ficar bom em desenvolvimento com Python!')
+            ]
