e8e2248c2539b15076d7d24e7cb369ea23716280
Luiz Fernando <luiz-fernando@outlook.com>
2022-10-21 15:43:00 -0300

    :rocket: Criada função que verifica destinatários e adiciona novo destinatário

--- brincadeira_com_mock/test_enviador_de_spam.py
+++ brincadeira_com_mock/test_enviador_de_spam.py
@@ -1,2 +1,12 @@
+from brincadeira_com_mock.enviador_de_spam import EnviadorDeSpam
+
+
 def test_criacao_enviador_de_spam():
-    assert EnviadorDeSpam is not None
+    assert EnviadorDeSpam() is not None
+
+
+def test_enviador_possui_destinatarios():
+    enviador = EnviadorDeSpam(['luiz@email.com'])
+    assert enviador.destinatarios == ['luiz@email.com']
+    enviador.adicionar_destinatario('fernando@email.com')
+    assert enviador.destinatarios == ['luiz@email.com', 'fernando@email.com']
